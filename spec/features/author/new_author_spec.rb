4ebad6f14ed48fceb3673aba8e2a8151bbfed2eb
MPaß <mpass99@gmx.de>
2020-11-11 15:42:32 +0100

    Copy&Paste guide spec

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -6,9 +6,13 @@ describe "New author page", type: :feature do
     visit new_author_path
   end
 
-  it "should contain input fields" do
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    visit new_author_path
+
+    # these are the standard names given to inputs by the form builder
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
 end
