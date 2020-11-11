e5cbdf77b93a8b607e7ff4fd3ad80afd746e33f2
MPaß <mpass99@gmx.de>
2020-11-11 15:26:16 +0100

    Add spec for new authors input fields

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,10 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit new_author_path
   end
+
+  it "should contain input fields" do
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
