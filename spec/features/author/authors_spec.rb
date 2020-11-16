7e2d45c07ffa72f410f6d97c6b8ce8da55437ac6
Maximilian Paß <maximilian.pass@student.hpi.de>
2020-11-16 18:04:42 +0100

    spec for link to edit page

--- spec/features/author/authors_spec.rb
+++ spec/features/author/authors_spec.rb
@@ -12,6 +12,13 @@ describe "authors index page", type: :feature do
     expect(page).to have_link 'New', href: new_author_path
   end
 
+  it "should have a link to create a new user" do
+    new_author = Author.create(first_name: "Alan", last_name: "Turing")
+    visit authors_path
+
+    expect(page).to have_link 'Edit', href: edit_author_path(new_author.reload.id)
+  end
+
   it "should have a table" do
     visit authors_path
 
