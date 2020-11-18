17886f2d09a71631e4bb05b58443d95c0f82a3b8
Maximilian Paß <maximilian.pass@student.hpi.de>
2020-11-18 23:40:28 +0100

    Add spec for link to delete

--- spec/features/author/authors_spec.rb
+++ spec/features/author/authors_spec.rb
@@ -12,11 +12,18 @@ describe "authors index page", type: :feature do
     expect(page).to have_link 'New', href: new_author_path
   end
 
-  it "should have a link to create a new user" do
+  it "should have a link to edit a user" do
     new_author = Author.create(first_name: "Alan", last_name: "Turing")
     visit authors_path
 
     expect(page).to have_link 'Edit', href: edit_author_path(new_author.reload.id)
+  end
+
+  it "should have a link to delete a user" do
+    new_author = Author.create(first_name: "Alan", last_name: "Turing")
+    visit authors_path
+
+    expect(page).to have_link 'Delete', href: edit_author_path(new_author.reload.id)
   end
 
   it "should have a table" do
